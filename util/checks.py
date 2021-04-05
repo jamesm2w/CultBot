a8f7d26136971e074b4ce3a2e4423dee705111da
Joeyh021 <josephharrison1@me.com>
2021-04-05 19:28:36 +0100

    added mod role to admin checks

--- util/checks.py
+++ util/checks.py
@@ -1,10 +1,9 @@
 import discord.ext.commands as commands
 
-ADMIN_ROLE_ID = 782307508261879829
-OWNER_ID = 330764682204020737
+ADMIN_ROLE_IDS = [782307508261879829, 795358205823549460, 330764682204020737]
 
 
 def admin_command():
     async def predicate(ctx):
-        return ctx.author.id == OWNER_ID or ctx.guild.get_role(ADMIN_ROLE_ID) in ctx.author.roles
+        return ctx.author.id in ADMIN_ROLE_IDS
     return commands.check(predicate)
