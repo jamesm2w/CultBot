00bf7e400d2cdcac4d7367a69ba02918f69d9fd1
Joeyh021 <josephharrison1@me.com>
2021-04-05 19:51:10 +0100

    changed admin check once again

--- util/checks.py
+++ util/checks.py
@@ -1,10 +1,9 @@
 import discord.ext.commands as commands
 
-ADMIN_ROLE_IDS = [782307508261879829, 795358205823549460]
 OWNER_ID = 330764682204020737
 
 
 def admin_command():
     async def predicate(ctx: commands.Context):
-        return ctx.author.id == OWNER_ID or [role.id for role in ctx.author.roles if role.id in ADMIN_ROLE_IDS] != []
+        return ctx.author.id == OWNER_ID or ctx.author.guild_permissions.administrator
     return commands.check(predicate)
