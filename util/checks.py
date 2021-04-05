52966ca67de4693877bb41bb56390164f7f0a3d3
Joeyh021 <josephharrison1@me.com>
2021-04-05 19:37:36 +0100

    fixed admin check

--- util/checks.py
+++ util/checks.py
@@ -1,9 +1,10 @@
 import discord.ext.commands as commands
 
-ADMIN_ROLE_IDS = [782307508261879829, 795358205823549460, 330764682204020737]
+ADMIN_ROLE_IDS = [782307508261879829, 795358205823549460]
+OWNER_ID = 330764682204020737
 
 
 def admin_command():
-    async def predicate(ctx):
-        return ctx.author.id in ADMIN_ROLE_IDS
+    async def predicate(ctx: commands.Context):
+        return ctx.author.id == OWNER_ID or [role.id for role in ctx.author.roles if role.id in ADMIN_ROLE_IDS] != []
     return commands.check(predicate)
